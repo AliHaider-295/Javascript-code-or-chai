f999761d0b394a0b076730cbf14377c9c7737718
AliHaider-295 <aleehaider953@gmail.com>
2024-08-25 14:09:07 +0000

    varible file add

---
+++ 01-basics/01_variable.js
@@ -0,0 +1,16 @@
+ const accountId = 123343;
+  let accountEmail = "aleehaider@gmail.com";
+  var accountPassword = "United295";
+  // accountId = 2; // not allowed
+  accountCity = "Karachi";
+    
+   console.log("accountId");
+    accountEmail = "alibhai@gmail.com";
+    accountPassword = "525253";
+    accountCity = "Lahore";
+    let accountState;
+    console.table([accountEmail , accountPassword, accountCity, accountState]);
+
+    /* prefor not use ver variable in your programming */
+    
+
