5cd82e4ecc930d04e7de36607476cf06d7069cda
AliHaider-295 <aleehaider953@gmail.com>
2024-08-25 14:45:07 +0000

    datatype

---
+++ 02_Datatype.js
@@ -0,0 +1,17 @@
+"use strict" // treat as new version of javascript.
+ // Data types.
+  // Number => 122334
+  // String => " Ali " any thing written in double codes or single code called string
+  // Bigint = bigint mostly use in large website like facebook.
+  // boolean = mostely use for islogin aur not is servies vaildfor me or not! ture/ false.
+  // Null  => null is also important datatype. use for standalone value.
+  // symbol =
+  // undfined =  unique 
+// are all primtive datetype.
+
+// null is object and its error is undefine always.
+// important questions asked for interview . Null is object in datatype.
+
+// Object
+// non primtive data ttype
+ console.log("Object")
